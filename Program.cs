a589f925a9c8da24792e92c02cadf7ebc6e6ce1b
Erick <erickblz@outlook.com>
2020-03-16 20:04:12 -0300

    Adicionando Exercício

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("O céu resplandece");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Ao meu redor, ao meu redor");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Vou voar, estrelas brilham entre as nuvens sem fim");
+        }
+    }
+}
